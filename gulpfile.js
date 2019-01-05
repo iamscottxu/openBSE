9ef5fa173d53cea5c8161ddea5c77592a838d5b4
徐誉诚 <scottxu@SCOTTXU-MBP>
2019-01-05 15:11:12 +0800

    gulp文件

--- gulpfile.js
+++ gulpfile.js
@@ -29,6 +29,7 @@ gulp.task('min', () => {
         .pipe(uglify())
         .pipe(rename('bulletScreenEngine.all.min.js'))
         .pipe(gulp.dest('dist'))
+        .pipe(gulp.dest('doc'))
 });
 
 gulp.task('default', gulp.series('build', 'min'));
