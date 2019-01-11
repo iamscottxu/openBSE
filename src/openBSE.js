03d811fcf0e01d6f3a1dc10d15ea5cdb5d798f80
徐誉诚 <scottxu@SCOTTXU-MBP>
2019-01-11 15:33:30 +0800

    增加命名空间

---
+++ src/openBSE.js
@@ -0,0 +1,75 @@
+/**
+ * openBSE 根命名空间
+ * @namespace
+ */
+let openBSE = {
+    BulletScreenEngine: require('./bulletScreenEngine').BulletScreenEngine,
+    BrowserNotSupportError: require('./browserNotSupportError').BrowserNotSupportError,
+    BulletScreenType: require('./bulletScreenType').BulletScreenType
+}
+
+export { openBSE }
+
+/**
+ * 全局选项
+ * @typedef {object} openBSE~Options
+ * @description Option 结构用于存放全局选项。
+ * @property {number} [verticalInterval=8] - 弹幕垂直行间距
+ * @property {number} [verticalInterval=1] - 弹幕播放速度（倍数）
+ * @property {openBSE~clockCallback} [clock=time => new Date().getTime() - startTime] - 时间基准：此时间基准可指向一个方法用于获取播放器当前进度，这个方法返回值即为播放进度（单位：毫秒）。
+ * @property {number} [scaling=1] 弹幕缩放比例（倍数）
+ * @property {openBSE~BulletScreenStyle} [defaultStyle] 默认弹幕样式：一个 {@link openBSE~BulletScreenStyle} 结构。
+ */
+
+/**
+ * 时间基准回调方法
+ * @callback openBSE~clockCallback
+ * @description ClockCallback 回调方法用于播放器当前进度。
+ * @returns {number} 播放进度：单位：毫秒。
+ */
+
+/**
+ * 单条弹幕数据
+ * @typedef {object} openBSE~BulletScreen
+ * @description BulletScreen 结构用于存放单条弹幕数据。
+ * @property {string} text 弹幕文本
+ * @property {boolean} [canDiscard=true] 是否允许丢弃：在弹幕过多时，程序将自动丢弃一些延迟过高的弹幕。此选项为 false 时本条弹幕无论如何都不会被丢弃，使用本选项的场景如本用户发送的弹幕。注意：不要将太多的弹幕的 canDiscard 设为 false， 否则会因超时的弹幕不会被丢弃而造成意外的问题。
+ * @property {number} [startTime=options.clock()] 弹幕进入时间：单位：毫秒，默认为[时间基准（options.clock）]{@link openBSE~Options}当前时间。
+ * @property {openBSE.BulletScreenType} [type=openBSE.BulletScreenType.rightToLeft] 弹幕类型：一个类型为 {@link openBSE.BulletScreenType} 的枚举。
+ * @property {openBSE~BulletScreenStyle} style 弹幕样式：一个 {@link openBSE~BulletScreenStyle} 结构。设置此选项中的任何一个值，将覆盖对应的全局设置。
+ */
+
+/**
+ * 弹幕样式
+ * @typedef {object} openBSE~BulletScreenStyle
+ * @description BulletScreenStyle 结构用于存放弹幕样式信息。
+ * @property {number} [shadowBlur=2] 弹幕阴影的模糊级别：0为不显示阴影。
+ * @property {string} [fontWeight="600"] 字体粗细：可选值：lighter：更细；normal：标准；bold：粗体；bolder: 更粗；100、200、300、400、500、600、700、800、900：定义由粗到细的字符（400 等同于 normal；700 等同于 bold）。
+ * @property {string} [fontFamily="sans-serif"] 字体系列：弹幕的字体族名称或/及类族名称的一个优先表。
+ * @property {number} [size=19] 字体大小：单位：像素。
+ * @property {string} [boxColor] 外框颜色：参照CSS颜色设置方法，为 null 不显示外框。
+ * @property {string} [color="white"] 弹幕颜色：参照CSS颜色设置方法，为 null 不显示此弹幕。
+ * @property {string} [borderColor] 描边颜色：参照CSS颜色设置方法，为 null 没有描边。
+ * @property {number} [speed=0.15] 弹幕速度：单位：像素/毫秒，仅弹幕类型为0、1时有效。
+ * @property {number} [residenceTime=5000] 弹幕停留时间：单位：毫秒，仅弹幕类型2、3时有效。
+ */
+
+/**
+ * 调试信息
+ * @typedef {object} openBSE~DebugInfo
+ * @description DebugInfo 结构用于存放调试信息。
+ * @property {number} time [时间基准（options.clock）]{@link openBSE~Options}当前时间。
+ * @property {number} bulletScreensOnScreenCount 实时弹幕总数
+ * @property {number} bulletScreensCount 剩余弹幕总数
+ * @property {number} delay 延迟：单位：毫秒。
+ * @property {number} delayBulletScreensCount 丢弃弹幕数：因延迟过高而丢弃的弹幕总数。
+ * @property {number} fps 帧频：单位：帧/秒。
+ */
+
+/**
+ * 版本信息
+ * @typedef {object} openBSE~VersionInfo
+ * @description VersionInfo 结构用于存放版本信息。
+ * @property {string} version 版本号
+ * @property {string} buildDate 构建日期：时区：UTC。
+ */
