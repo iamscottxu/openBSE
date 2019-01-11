f3fcd27ff0ecdb4b37984869978228bde7dacac7
徐誉诚 <scottxu@SCOTTXU-MBP>
2019-01-11 15:33:44 +0800

    弹幕类型

---
+++ src/bulletScreenType.js
@@ -0,0 +1,18 @@
+/**
+ * 弹幕类型枚举
+ * @alias openBSE.BulletScreenType
+ * @readonly
+ * @enum {number}
+ */
+const BulletScreenType = {
+    /** 从右到左弹幕 */
+    rightToLeft: 1,
+    /** 从左到右弹幕（逆向弹幕） */
+    leftToRight: 2,
+    /** 顶部弹幕 */
+    top: 4,
+    /** 底部弹幕 */
+    bottom: 7
+}
+
+export { BulletScreenType }
