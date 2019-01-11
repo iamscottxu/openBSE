ba6ab80279ca9483063526766db45f7fd89ccb55
徐誉诚 <scottxu@SCOTTXU-MBP>
2019-01-11 15:28:13 +0800

    构建配置

--- gulpfile.js
+++ gulpfile.js
@@ -7,35 +7,36 @@ const rename = require("gulp-rename");
 const header = require('gulp-header');
 const jsdoc = require('gulp-jsdoc3');
 const fs = require('fs-extra');
-const VERSION = '2.0-Alpha'; //版本号
+const buildConfig = require('./build.json');
 
 gulp.task('doc', function (cb) {
-    gulp.src(['README.md', 'LICENSE', 'src/bulletScreenEngine.js'], {read: false})
-        .pipe(jsdoc(cb));
+    let config = require('./jsdoc.json');
+    config.opts.destination = `./docs/${buildConfig.version}`;
+    gulp.src(['README.md', 'src/*.js'], {read: false})
+        .pipe(jsdoc(config, cb));
 });
 
 gulp.task('build', () => {
     let version = '';
-    version += 'const VERSION = "' + VERSION + '";';
-    version += 'const BUILE_DATE = "' + new Date().toUTCString() + '";';
+    version += `const VERSION = '${buildConfig.version}';`;
+    version += `const BUILE_DATE = '${new Date().toUTCString()}';`;
     version += 'export {VERSION, BUILE_DATE}';
     fs.writeFileSync('./src/version.js', version, { 'flag': 'w' });
     let license = fs.readFileSync('./LICENSE').toString();
-    let home = 'https://iamscottxu.github.io/BulletScreenEngine/';
     return browserify({
-        entries: ['src/app.js', 'src/bulletScreenEngine.js']
+        entries: ['src/app.js', 'src/openBSE.js']
     })
         .transform(babelify, {
             presets: ['@babel/preset-env']
         })
         .bundle()
-        .pipe(source('bulletScreenEngine.all.js'))
-        .pipe(header('/*!\n${license}\nHome: ${home}\n*/\n', { license: license, home: home }))
+        .pipe(source(`${buildConfig.name}.all.js`))
+        .pipe(header('/*!\n${license}\nHome: ${home}\n*/\n', { license: license, home: buildConfig.home }))
         .pipe(gulp.dest('dist'))
 });
 
 gulp.task('min', () => {
-    return gulp.src('dist/bulletScreenEngine.all.js')
+    return gulp.src(`dist/${buildConfig.name}.all.js`)
         .pipe(uglify({
             output: {
                 comments: '/^!/'
