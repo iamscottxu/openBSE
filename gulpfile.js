678f0924c3c0c7c10875bb8ff1e120e600752e52
徐誉诚 <scottxu@SCOTTXU-MBP>
2019-01-12 13:35:01 +0800

    修改gulp文件

--- gulpfile.js
+++ gulpfile.js
@@ -7,42 +7,53 @@ const rename = require("gulp-rename");
 const header = require('gulp-header');
 const jsdoc = require('gulp-jsdoc3');
 const fs = require('fs-extra');
+const sourcemaps = require('gulp-sourcemaps');
+const buffer = require('vinyl-buffer');
 const buildConfig = require('./build.json');
 
 gulp.task('doc', function (cb) {
     let config = require('./jsdoc.json');
     config.opts.destination = `./docs/${buildConfig.version}`;
-    gulp.src(['README.md', 'src/*.js'], {read: false})
+    gulp.src(['README.md', 'src/*.js'], { read: false })
         .pipe(jsdoc(config, cb));
 });
 
 gulp.task('build', () => {
-    let version = '';
-    version += `const VERSION = '${buildConfig.version}';`;
-    version += `const BUILE_DATE = '${new Date().toUTCString()}';`;
-    version += 'export {VERSION, BUILE_DATE}';
-    fs.writeFileSync('./src/version.js', version, { 'flag': 'w' });
+    fs.writeFileSync('./src/buildDate.json',
+        JSON.stringify({
+            buildDate: new Date().toUTCString()
+        }), { 'flag': 'w' });
     let license = fs.readFileSync('./LICENSE').toString();
     return browserify({
-        entries: ['src/app.js', 'src/openBSE.js']
+        entries: 'src/app.js',
+        debug: true
     })
         .transform(babelify, {
-            presets: ['@babel/preset-env']
+            presets: ['@babel/preset-env'],
+            shouldPrintComment: (val) => /^\*/.test(val)
         })
         .bundle()
         .pipe(source(`${buildConfig.name}.all.js`))
+        .pipe(buffer())
+        .pipe(sourcemaps.init({ loadMaps: true }))
         .pipe(header('/*!\n${license}\nHome: ${home}\n*/\n', { license: license, home: buildConfig.home }))
+        .pipe(sourcemaps.write('./'))
         .pipe(gulp.dest('dist'))
 });
 
 gulp.task('min', () => {
     return gulp.src(`dist/${buildConfig.name}.all.js`)
+        .pipe(sourcemaps.init({ loadMaps: true }))
         .pipe(uglify({
+            compress: {
+                passes: 10
+            },
             output: {
                 comments: '/^!/'
             }
         }))
         .pipe(rename({ suffix: '.min' }))
+        .pipe(sourcemaps.write('./'))
         .pipe(gulp.dest('dist'))
 });
 
