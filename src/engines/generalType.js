6dfdb738aeaa452ea2833f325fe05813d33b7246
scottxu <scottxu@scottxublog.com>
2019-05-08 23:18:25 +0800

    重命名并修改

---
+++ src/engines/generalType.js
@@ -0,0 +1,30 @@
+/**
+ * 弹幕类型枚举
+ * @alias openBSE.BulletScreenType
+ * @readonly
+ * @enum {number}
+ */
+const GeneralType = {
+    /** 
+     * 从右到左弹幕
+     * @readonly
+     */
+    rightToLeft: 1,
+    /** 
+     * 从左到右弹幕（逆向弹幕）
+     * @readonly
+     */
+    leftToRight: 2,
+    /** 
+     * 顶部弹幕
+     * @readonly
+     */
+    top: 4,
+    /** 
+     * 底部弹幕
+     * @readonly
+     */
+    bottom: 8
+}
+
+export { GeneralType }
