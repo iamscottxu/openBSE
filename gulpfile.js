2508af2e658944e657105e6084bce1b9494077e9
徐誉诚 <xyc0714@aliyun.com>
2020-09-03 17:23:51 +0800

    更新构建脚本

--- gulpfile.js
+++ gulpfile.js
@@ -5,11 +5,12 @@ const source = require('vinyl-source-stream');
 const rename = require("gulp-rename");
 const header = require('gulp-header');
 const jsdoc = require('gulp-jsdoc3');
+const jeditor = require('gulp-json-editor');
 const fs = require('fs-extra');
 const sourcemaps = require('gulp-sourcemaps');
 const buffer = require('vinyl-buffer');
 const babel = require("gulp-babel");
-const jshint = require("gulp-jshint");
+const eslint = require('gulp-eslint');
 const buildConfig = require('./build.json');
 
 gulp.task('doc', function (cb) {
@@ -19,11 +20,18 @@ gulp.task('doc', function (cb) {
         .pipe(jsdoc(config, cb));
 });
 
-gulp.task('es6', () => {
-    buildConfig.buildDate = new Date().toUTCString();
-    fs.writeJSONSync('dist/build.json', buildConfig);
-    fs.copyFileSync('src/lib/resources.json', 'dist/lib/resources.json');
-    return gulp.src(['src/**/*.js', '!src/JS-Interpreter/acorn_interpreter.js', '!src/JS-Interpreter/demos/*.js'])
+gulp.task('es6', gulp.series(gulp.parallel(() => {
+    return gulp.src("./build.json")
+    .pipe(jeditor({
+        'buildDate': new Date().toUTCString()
+    }))
+    .pipe(gulp.dest('dist'))
+}, () => {
+    return gulp.src('src/lib/resources.json')
+      .pipe(gulp.dest('dist/lib'))
+}),
+() => {
+    return gulp.src(['src/**/*.js', '!src/lib/JS-Interpreter/acorn_interpreter.js', '!src/lib/JS-Interpreter/demos/**/*'])
         .pipe(sourcemaps.init())
         .pipe(babel({
             presets: [
@@ -39,7 +47,7 @@ gulp.task('es6', () => {
         }))
         .pipe(sourcemaps.write())
         .pipe(gulp.dest('dist'));
-});
+}));
 
 gulp.task('build', () => {
     let license = fs.readFileSync('./LICENSE').toString();
@@ -73,10 +81,20 @@ gulp.task('min', () => {
         .pipe(gulp.dest('dist'))
 });
 
-gulp.task('jshint', () => {
-    return gulp.src('src/js/*.js')
-        .pipe(jshint())
-        .pipe(jshint.reporter('default'))
+gulp.task('eslint', () => {
+    return gulp.src(['src/*.js', 'src/*.ts'])
+        .pipe(eslint())
+        .pipe(eslint.format())
+        .pipe(eslint.failAfterError())
 });
 
-gulp.task('default', gulp.series('es6', 'build', 'min', 'doc'));
+gulp.task('copy', () => {
+    return gulp.src('src/openBSE.d.ts')
+      .pipe(gulp.dest('dist'))
+});
+
+gulp.task('default', gulp.parallel(
+    gulp.series('es6', 'build', 'min'), 
+    gulp.series('doc'), 
+    gulp.series('copy'))
+);
