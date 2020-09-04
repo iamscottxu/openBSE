85dad05501d5f02c4d9798248a5bae235ec7384c
徐誉诚 <xyc0714@aliyun.com>
2020-09-04 09:05:39 +0800

    更新编译脚本

--- gulpfile.js
+++ gulpfile.js
@@ -16,7 +16,7 @@ const buildConfig = require('./build.json');
 gulp.task('doc', function (cb) {
     let config = require('./jsdoc.json');
     config.opts.destination = `./docs/${buildConfig.version}`;
-    gulp.src(['docs/README.md', 'src/*.js'], { read: false })
+    gulp.src(['docs/README.md', 'src/**/*.js', '!src/lib/**/*', '!src/renderers/**/*'], { read: false })
         .pipe(jsdoc(config, cb));
 });
 
